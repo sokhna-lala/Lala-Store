2d652d976697cb11be31f4cea4f2fb0cc3168f60
sokhna lala sarr <sokhnalala97@gmail.com>
2025-10-08 16:30:08 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="h-screen flex items-center justify-center bg-gradient-to-r from-blue-400 to-purple-500">
+        <h1 className="text-4xl font-bold text-white shadow-lg p-6 rounded-2xl bg-white/10 backdrop-blur-sm">
+          Bonjour tout le monde 👋
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
