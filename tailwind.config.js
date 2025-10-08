0fa66e3fa0d87b33e23512cf715a7e9b94a0d474
sokhna lala sarr <sokhnalala97@gmail.com>
2025-10-08 21:55:29 +0200

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html", // pour Vite (ou le fichier racine HTML)
+    "./src/**/*.{js,ts,jsx,tsx}", // pour tous les fichiers React dans src/
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
