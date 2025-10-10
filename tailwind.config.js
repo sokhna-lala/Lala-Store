5ac8d54a310916250632b9b6aef735d0133aeaf9
23CISS33 <cissndeyebineta149@gmail.com>
2025-10-10 15:06:41 +0000

    Ajout du projets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-
-/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html", // pour Vite (ou le fichier racine HTML)
     "./src/**/*.{js,ts,jsx,tsx}", // pour tous les fichiers React dans src/
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brown: {
+          100: '#f7f3ef',
+          400: '#d4b18f',
+          600: '#b48b63', // couleur principale
+        },
+      },
+    },
   },
   plugins: [],
 };
